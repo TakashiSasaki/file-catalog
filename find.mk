1371ca2a4fb460e95124441ee316ab05633ea2c2
Takashi SASAKI <takashi316@gmail.com>
2019-01-05 00:29:59 +0900

    use jq transpose filter

--- find.mk
+++ find.mk
@@ -10,11 +10,29 @@ ifndef XARGS_COMMAND
 	XARGS_COMMAND:=xargs
 endif
 
-%.depth1.dirs: %.dirs
-	cat $< | $(XARGS_COMMAND) -n 1 -I{} $(FIND_COMMAND) {} -maxdepth 1 -mindepth 1 | tee $@
+%.depth1.dirs: %.depth0.dirs
+	cat $< | $(XARGS_COMMAND) -n 1 -I{} $(FIND_COMMAND) {} -maxdepth 1 -mindepth 1 -type d | tee $@
 	test -s $@
 
 %.depth2.dirs: %.depth1.dirs
-	cat $< | $(XARGS_COMMAND) -n 1 -I{} $(FIND_COMMAND) {} -maxdepth 1 -mindepth 1 | tee $@
+	cat $< | $(XARGS_COMMAND) -n 1 -I{} $(FIND_COMMAND) {} -maxdepth 1 -mindepth 1 -type d | tee $@
 	test -s $@
 
+
+%.dirs.ldjson: %.dirs
+	jq -R . $< | tee $@
+
+%.json: %.ldjson
+	jq -s . $< | tee $@
+
+%.md5.ldjson: %.md5
+	jq -R . $< | tee $@
+
+%.ldjson.md5: %.ldjson
+	-rm $@
+	cat $< | $(XARGS_COMMAND) -n 1 -I {} sh -c "echo -n {} | md5sum | awk '{print \$$1}' | tee -a $@"
+
+%.dirs.x: %.dirs.json %.dirs.ldjson.md5.json
+	jq -s -c 'transpose|.[]' $^ | tee $@
+
+all: root.depth1.dirs.x

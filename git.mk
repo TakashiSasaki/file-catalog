2a25b4c9637dc1cbea41ad06f41971370344b3f5
Takashi SASAKI <takashi316@gmail.com>
2019-01-04 13:13:42 +0900

    make git-all to get git metadata files

--- git.mk
+++ git.mk
@@ -1,12 +1,26 @@
+git-all: .git-rev-parse-all \
+	.git-ls-tree \
+	.git-check-attr \
+	.git-config-remote \
+	.git-config-branch \
+	.gitdir
 
-git-rev-parse-all.githash:
+.git-rev-parse-all:
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
 	unset GIT_DIR; \
 	       	git rev-parse --all | uniq | tee $@
 	test -s $@
 
-git-ls-tree.txt: git-rev-parse-all.githash
+.git-ls-tree: .git-rev-parse-all
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
 	unset GIT_DIR; \
-	cat $< | xargs -n 1 git -C ls-tree | tee  $@
+	cat $< | xargs -n 1 git ls-tree | tee  $@
 	test -s $@
 
 git-config:
@@ -20,11 +34,33 @@ git-config:
 	git config pager.diff ""
 	git config pager.reflog ""
 
-git-check-attr:
-	find . | git check-attr --stdin -a
+.git-check-attr:
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
+	find . | git check-attr --stdin -a | tee $@
 
-git-config-remote.txt:
+.git-config-remote:
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
 	git config -l | grep '^remote\.' | tee $@
 
-git-config-branch.txt:
+.git-config-branch:
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
 	git config -l | grep '^branch\.' | tee $@
+
+.gitdir:
+	echo $@ merge=ours>>.gitattributes
+	sort -u -o .gitattributes .gitattributes
+	echo $@ >>.gitignore
+	sort -u -o .gitignore .gitignore
+	git rev-parse --git-dir | tee $@
+
+
+
